3c771fafa66a3bcd874104742d551d288736d4b9
kaiser24 <damsog38@gmail.com>
2023-10-04 14:38:24 -0500

    Player attack animations

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,13 +5,31 @@ extends CharacterBody2D
 @export var friction : float = 500
 @export var starting_position : Vector2 = Vector2(0,0)
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 @onready var animation_tree = $AnimationTree
 @onready var animation_state = animation_tree.get("parameters/playback")
 
 func _ready():
+	animation_tree.active = true
 	update_animation_parameters(starting_position)
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+	
+func move_state(delta):
 	# Get input direction
 	# When pressing a key we set the input vector that describes the desired movement
 	var input_direction : Vector2 = Vector2(
@@ -29,12 +47,24 @@ func _physics_process(delta):
 		#If the keys are not pressed the player will tend to stop with a friction 
 		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		
+	# Attack State Transition
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
 	
 	# Move and slide
 	print(velocity)
 	move_and_slide()
 
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animation_state.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
+
 func update_animation_parameters(move_input: Vector2):
 	if(move_input != Vector2.ZERO):
 		animation_tree.set("parameters/Idle/blend_position", move_input)
 		animation_tree.set("parameters/Run/blend_position", move_input)
+		animation_tree.set("parameters/Attack/blend_position", move_input)
