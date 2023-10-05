ff60117170ebe34bcbdc32c3ff70d140f8101e62
kaiser24 <damsog38@gmail.com>
2023-10-05 15:14:04 -0500

    Refactoring and enemy death effect

--- Enemies/bat.gd
+++ Enemies/bat.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 @onready var stats = $Stats
 
+const EnemyDeathEffect = preload("res://Effects/enemy_death_effect.tscn")
+
 func _physics_process(delta):
 	# Knockback
 	velocity = velocity.move_toward(Vector2.ZERO, 200 * delta)
@@ -13,3 +15,6 @@ func _on_hurtbox_area_entered(area):
 
 func _on_stats_no_health():
 	queue_free()
+	var enemy_death_effect = EnemyDeathEffect.instantiate()
+	get_parent().add_child(enemy_death_effect)
+	enemy_death_effect.global_position = position
--- World/grass.gd
+++ World/grass.gd
@@ -1,10 +1,9 @@
 extends Sprite2D
+const GrassEffect = preload("res://Effects/grass_effect.tscn")
 
 func create_grass_effect():
-	var GrassEffect = load("res://Effects/grass_effect.tscn")
 	var grass_effect = GrassEffect.instantiate()
-	var world = get_tree().current_scene
-	world.add_child(grass_effect)
+	get_parent().add_child(grass_effect)
 	grass_effect.global_position = global_position
 
 func _on_hurtbox_area_entered(area):
